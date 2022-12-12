7700f65047ec3ddfd0bb56cdb3274d85e7871623
Pawel Rybka <rrybka1999@gmail.com>
2022-12-12 23:18:45 +0100

    Adding input

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,37 +1,53 @@
-import React from "react";
+import React from 'react'
 import TodoItem from './TodoItem'
 
 
 class Todo extends React.Component {
     state = { 
         elements: [
-            {id:'3218732', isCompleted: false, title:'Wysrać się'},
-            {id:'9420521', isCompleted: true, title:'Puścić bąksa'},
-            {id:'7641203', isCompleted: false, title:'Wypić pącz'}
-        ]  
+            {id:'4326346', icCompleted: true, title: 'Sranie na zawołanie'},
+            {id:'9539534', isCompleted: false, title: 'Bączenie'},
+            {id:'2905706', isCompleted: false, title: 'Wypić pącz'},
+            {id:'9827356', isCompleted: false, title: 'Nasrać do schowka'}
+        ],
+        inputValue: '' 
     }
     
-    markCompleted(id) {
+    markClicked(id) {
         const index = this.state.elements.findIndex(x => x.id == id)
         const newElements = this.state.elements
-        newElements[index].isCompleted = true
-        
-        this.setState( { elements:newElements })
+        newElements[index].isCompleted = true;
     
+        this.setState({ elements: newElements})
     }
 
+    addItem() {
+        const item = {
+            id: Math.random(),
+            title: this.state.inputValue
+        }
+        const newElements = [item, ...this.state.elements]
+        this.setState({ elements: newElements })
+    }
+
+    inputHandler(event) {
+        const newValue = event.target.value
+        this.setState({ inputValue: newValue})
+    }
+
+
     render() {
-        const elements = this.state.elements.map(e => {
-           return <TodoItem element={e} markClicked={this.markCompleted.bind(this)} />
+        const elements = this.state.elements.map(element => {
+            return <TodoItem element={element} markCompleted ={this.markClicked.bind(this)}/>
         })
-    
-        return(
-            <div>
-                Todo App
-                <input type='text'/>
-                <button>Dodaj do listy</button>
+
+        return (
+            <div className='all'>
+                <div>Todo application</div>
+                <input type='text' placeholder='New Task' value={this.state.inputValue} onChange={this.inputHandler.bind(this)}/>
+                <button onClick={this.addItem.bind(this)}>Add Task</button>
                 {elements}
-            </div>
+            </div>          
         )
     }
 }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,13 +1,15 @@
 import React from "react";
 
-const TodoItem = props => {
-    const markHandler = () => props.markClicked(props.element.id)
+const TodoItem = props =>  {
+    const markClicked = () => props.markCompleted(props.element.id)
     return (
-        <div className={`card ${props.element.isCompleted ? 'completed' : ''}`} key={props.element.id}>
+        <div className={`card ${props.element.isCompleted ? 'completed' : ''}`}  key={props.element.id}>
             <h2>{props.element.title}</h2>
-            <button onClick={markHandler}  className="tak">Zakończone</button>
+            <button onClick={markClicked} className="tak">Zakończone</button>
         </div>
     )
 }
 
+
 export default TodoItem
+
